7dcf1b96f240b43b2964e14c90c14070b613cb2d
Sasha Bayan <SashaBayan@gmail.com>
2015-06-04 10:39:42 -0700

    adds es6 and type examples

--- main.ts
+++ main.ts
@@ -1,4 +1,57 @@
 console.log('Hello World')
 
+///////////////////ES6 Features
+
+//Classes
 class Test {}
 var t = new Test();
+
+//Destructuring
+function displayPerson(p) {
+    let {name, age} = p;
+    console.log(name, age);
+}
+
+var john = { name: "John", age: 20 };
+displayPerson(john); 
+
+//Swapping
+var a = 1;
+var b = 2;
+
+[b, a] = [a, b];
+console.log(a, b)
+
+//Let + Const
+let shmoosh = "shmoosh"
+console.log(shmoosh)
+const Gaia = "Gaia"
+
+
+//////////////Types
+
+//Boolean
+var truth: boolean = true
+
+//Number
+var six: number = 6;
+
+//String
+var name: string = "foo";
+name = "bar"
+
+//Any Type
+var notSure: any = 4;
+notSure = "maybe a string instead";
+notSure = false; 
+
+var list: any[] = [1, true, "free"];
+
+list[1] = 100;
+
+//Void type
+function sayHi(): void {
+    console.log("hi");
+}
+
+
