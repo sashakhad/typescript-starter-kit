b3f4a2bd48fa4f3953916dd9666a7fa5902f82ee
Sasha Bayan <SashaBayan@gmail.com>
2015-06-15 19:25:40 -0700

    adds generator example

--- main.ts
+++ main.ts
@@ -27,11 +27,29 @@ let shmoosh = "shmoosh"
 console.log(shmoosh)
 const Gaia = "Gaia"
 
+let nums = [1, 2, 3]
+console.log(nums)
+console.log(...nums) 
+
+//Generators
+function * idMaker() {
+    var index = 0;
+    while (index < 3)
+        yield index++;
+}
+
+var gen = idMaker();
+
+console.log(gen.next().value); // 0
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 2
+console.log(gen.next().value); // undefined
 
 //////////////Types
 
 //Boolean
 var truth: boolean = true
+truth = "truth"
 
 //Number
 var six: number = 6;
@@ -39,6 +57,8 @@ var six: number = 6;
 //String
 var name: string = "foo";
 name = "bar"
+
+
 
 //Any Type
 var notSure: any = 4;
@@ -50,8 +70,9 @@ var list: any[] = [1, true, "free"];
 list[1] = 100;
 
 //Void type
-function sayHi(): void {
+var sayHi = function(): void {
     console.log("hi");
 }
 
+sayHi = "hi"
 
